9c7ac4ca864d07ba3642ca15ff1f0224b97fea30
Frederic Linn <fredericlinn@gmail.com>
2020-01-16 23:14:53 +0100

    Refactor Input / Output
    The input model was simplified so that there's only a
    very thin wrapper above the functionality of the minifb crate
    in order to deal with the chip-8's special hex-keyboard.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use minifb::{Key, Window, WindowOptions, Scale};
+use minifb::{Key, Scale, Window, WindowOptions};
 use rand::{thread_rng, Rng};
 
 #[allow(dead_code)]
-#[derive(Clone, Copy)]
-struct Cpu {
+struct Chip8 {
     // program counter,
     pc: u16,
     // index register
@@ -20,26 +19,15 @@ struct Cpu {
     dt: u8,
     // sound timer
     st: u8,
-    // framebuffer
-    display: Display,
-    // keyboard
-    keys: [bool; 16],
+    // Input / Output
+    io: Io,
 }
 
-// Split the two bytes of an opcode into four nibbles.
-fn split_opcode(opcode: u16) -> (u8, u8, u8, u8) {
-    (
-        ((opcode & 0xF000) >> 12) as u8,
-        ((opcode & 0x0F00) >> 8) as u8,
-        ((opcode & 0x00F0) >> 4) as u8,
-        (opcode & 0x000F) as u8,
-    )
-}
 
 #[allow(dead_code)]
-impl Cpu {
-    fn new() -> Cpu {
-        Cpu {
+impl Chip8 {
+    fn new() -> Chip8 {
+        Chip8 {
             pc: 0,
             i: 0,
             memory: [0; 4096],
@@ -48,9 +36,17 @@ impl Cpu {
             sp: 0,
             dt: 0,
             st: 0,
-            display: Display::new(),
-            keys: [false; 16],
+            io: Io::new(),
         }
+    }
+    // Split the two bytes of an opcode into four nibbles.
+    fn split_opcode(opcode: u16) -> (u8, u8, u8, u8) {
+        (
+            ((opcode & 0xF000) >> 12) as u8,
+            ((opcode & 0x0F00) >> 8) as u8,
+            ((opcode & 0x00F0) >> 4) as u8,
+            (opcode & 0x000F) as u8,
+        )
     }
 
     fn init_memory(&mut self) {
@@ -61,17 +57,17 @@ impl Cpu {
         self.pc = 0x200;
         self.i = 0;
         self.init_memory();
-        self.v =  [0; 16];
+        self.v = [0; 16];
         self.stack = [0; 16];
-        self.sp =  0;
-        self.dt =  0;
+        self.sp = 0;
+        self.dt = 0;
         self.st = 0;
-        self.display.cls();
-        self.keys = [false; 16];
+        self.io.cls();
     }
 
     fn execute_cycle(&mut self) {
-        let opcode = (self.memory[self.pc as usize] as u16) << 8 | (self.memory[(self.pc + 1) as usize] as u16);
+        let opcode = (self.memory[self.pc as usize] as u16) << 8
+            | (self.memory[(self.pc + 1) as usize] as u16);
         self.process_opcode(opcode);
     }
 
@@ -88,12 +84,11 @@ impl Cpu {
         // jump instructions will overwrite the value anyway.
         self.pc += 2;
 
-        let nibbles = split_opcode(opcode);
+        let nibbles = Chip8::split_opcode(opcode);
 
         match nibbles {
-
             // 00E0 Clear the screen
-            (0x0, 0x0, 0xE, 0x0) => self.display.cls(),
+            (0x0, 0x0, 0xE, 0x0) => self.io.cls(),
 
             // 00EE Return from a subroutine
             // pop old pc form stack
@@ -108,13 +103,13 @@ impl Cpu {
 
             // 1NNN Jump to address NNN
             (0x1, _, _, _) => self.pc = nnn,
-           
+
             // 2NNN Execute subroutine starting at address NNN
             (0x2, _, _, _) => {
                 self.stack[self.sp as usize] = self.pc;
                 self.sp += 1;
                 self.pc = nnn;
-            },
+            }
 
             // 3XNN Skip the following instruction if the value of register VX equals NN
             (0x3, _, _, _) => self.pc += if vx == nn { 2 } else { 0 },
@@ -142,21 +137,25 @@ impl Cpu {
 
             // 8XY3 Set VX to VX XOR VY
             (0x8, _, _, 0x3) => self.v[x] = vx ^ vy,
-           
+
             // 8XY4 Add the value of register VY to register VX
             // Set VF to 01 if a carry occurs
             // Set VF to 00 if a carry does not occur
             (0x8, _, _, 0x4) => {
                 let (_, overflow) = self.v[x].overflowing_add(vy);
-                if overflow { self.v[x] = 1 } ;
-            },
+                if overflow {
+                    self.v[x] = 1
+                };
+            }
 
             // 8XY5 Subtract the value of register VY from register VX
             // Set VF to 00 if a borrow occurs
             // Set VF to 01 if a borrow does not occur
             (0x8, _, _, 0x5) => {
                 let (_, overflow) = self.v[x].overflowing_sub(vy);
-                if overflow { self.v[x] = 0 };
+                if overflow {
+                    self.v[x] = 0
+                };
             }
 
             // 8XY6 Store the value of register VY shifted right one bit in register VX
@@ -165,7 +164,7 @@ impl Cpu {
                 let lsb = vy & 0x1;
                 self.v[x] = vy >> 1;
                 self.v[0xF] = lsb;
-            },
+            }
 
             // 8XY7 Set register VX to the value of VY minus VX
             // Set VF to 00 if a borrow occurs
@@ -173,8 +172,10 @@ impl Cpu {
             (0x8, _, _, 0x7) => {
                 let (val, overflow) = vy.overflowing_sub(vx);
                 self.v[x] = val;
-                if overflow { self.v[0xF] = 0};
-            },
+                if overflow {
+                    self.v[0xF] = 0
+                };
+            }
 
             // 8XYE Store the value of register VY shifted left one bit in register VX
             // Set register VF to the most significant bit prior to the shift
@@ -182,7 +183,7 @@ impl Cpu {
                 let msb = vy & 0x80;
                 self.v[x] = vy << 1;
                 self.v[0xF] = msb;
-            },
+            }
 
             // 9XY0 Skip the following instruction if the value of register VX is not equal to the value of register VY
             (0x9, _, _, 0x0) => self.pc += if vx != vy { 2 } else { 0 },
@@ -195,27 +196,35 @@ impl Cpu {
 
             // CXNN Set VX to a random number with a mask of NN
             (0xC, _, _, _) => {
-
                 let mut rng = thread_rng();
                 let rn: u8 = rng.gen();
                 self.v[x] = rn & nn;
-            },
+            }
 
             // DXYN Draw a sprite at position VX, VY with N bytes of sprite data starting at the address stored in I
             // Set VF to 01 if any set pixels are changed to unset, and 00 otherwise
-            (0xD, _, _, _) => self.display.draw(),
+            (0xD, _, _, _) => self.io.draw(),
 
             // EX9E Skip the following instruction if the key corresponding to the hex value currently stored in register VX is pressed
-            (0xE, _, _, 0xE) => self.pc += if self.keys[vx as usize] { 2 } else { 0 },
+            (0xE, _, _, 0xE) => self.pc += if self.io.is_key_pressed(vx) { 2 } else { 0 },
 
             // EXA1 Skip the following instruction if the key corresponding to the hex value currently stored in register VX is not pressed
-            (0xE, _, 0xA, 0x1) => self.pc += if !self.keys[vx as usize] { 2 } else { 0 },
+            (0xE, _, 0xA, 0x1) => self.pc += if !self.io.is_key_pressed(vx) { 2 } else { 0 },
 
             // FX07 Store the current value of the delay timer in register VX
             (0xF, _, 0x0, 0x7) => self.v[x] = self.dt,
 
             // FX0A Wait for a keypress and store the result in register VX
             (0xF, _, 0x0, 0xA) => {
+                // https://github.com/ColinEberhardt/wasm-rust-chip8
+                // I love the simplicity of just subtracting from the program counter.
+                self.pc -= 2;
+                for key in 0x0..=0xF {
+                    if self.io.is_key_pressed(key) {
+                        self.v[x] = key;
+                        self.pc += 2;
+                    }
+                }
             },
 
             // FX15 Set the delay timer to the value of register VX
@@ -231,7 +240,11 @@ impl Cpu {
             (0xF, _, 0x2, 0x9) => self.i = vx as u16 * 5,
 
             // FX33 Store the binary-coded decimal equivalent of the value stored in register VX at addresses I, I+1, and I+2
-            (0xF, _, 0x3, 0x3) => (),
+            (0xF, _, 0x3, 0x3) => {
+                self.memory[self.i as usize] = vx / 100;
+                self.memory[self.i as usize + 1] = (vx / 10) % 10;
+                self.memory[self.i as usize + 2] = (vx % 100) % 10;
+            }
 
             // FX55 Store the values of registers V0 to VX inclusive in memory starting at address I
             // I is set to I + X + 1 after operation
@@ -244,7 +257,11 @@ impl Cpu {
 
             // FX65 Fill registers V0 to VX inclusive with the values stored in memory starting at address I
             // I is set to I + X + 1 after operation
-            (0xF, _, 0x6, 0x5) => (),
+            (0xF, _, 0x6, 0x5) => {
+                for r in 0..=x {
+                    self.v[r] = self.memory[(self.i + r as u16) as usize];
+                }
+            }
 
             // Gotta catch 'em all!
             (_, _, _, _) => (),
@@ -252,116 +269,137 @@ impl Cpu {
     }
 }
 
-pub trait HexToKey{
-    fn is_hex_pressed(&self, hex_key: u8) -> bool;
+const WIDTH: usize = 64;
+const HEIGHT: usize = 32;
+#[allow(dead_code)]
+const KEY_MAP: [Key; 16] = [
+    Key::Key1,
+    Key::Key2,
+    Key::Key3,
+    Key::Key4,
+    Key::Q,
+    Key::W,
+    Key::E,
+    Key::R,
+    Key::A,
+    Key::S,
+    Key::D,
+    Key::F,
+    Key::Y,
+    Key::X,
+    Key::C,
+    Key::V,
+];
+
+
+struct Io {
+    framebuffer: [u32; WIDTH * HEIGHT],
+    window: Window,
+    keys: [bool; 16],
 }
 
-impl HexToKey for Window {
-    fn is_hex_pressed(&self, hex_key: u8) -> bool {
-        match hex_key {
-            0x0 => self.is_key_down(Key::Key1),
-            0x1 => self.is_key_down(Key::Key2),
-            0x2 => self.is_key_down(Key::Key3),
-            0x3 => self.is_key_down(Key::Key4),
-            0x4 => self.is_key_down(Key::Q),
-            0x5 => self.is_key_down(Key::W),
-            0x6 => self.is_key_down(Key::E),
-            0x7 => self.is_key_down(Key::R),
-            0x8 => self.is_key_down(Key::A),
-            0x9 => self.is_key_down(Key::S),
-            0xA => self.is_key_down(Key::D),
-            0xB => self.is_key_down(Key::F),
-            0xC => self.is_key_down(Key::Y), // assumes QWERTZ
-            0xD => self.is_key_down(Key::X),
-            0xE => self.is_key_down(Key::C),
-            0xF => self.is_key_down(Key::V),
-            _ => false,
-        }
-    }
-}
-
-#[derive(Clone, Copy)]
-struct Display {
-    framebuffer: [u32; 64 * 32],
-}
 
 #[allow(dead_code)]
-impl Display {
-   fn new() -> Display {
-        Display {
-            framebuffer: [0; 64 * 32],
+impl Io {
+    fn new() -> Io {
+        Io {
+            keys: [false; 16],
+            framebuffer: [0; WIDTH * HEIGHT],
+            window: Window::new(
+                "Crusty-8 (Press ESC to exit)",
+                WIDTH,
+                HEIGHT,
+                WindowOptions {
+                    resize: false,
+                    scale: Scale::X16,
+                    ..WindowOptions::default()
+                },
+            )
+            .unwrap_or_else(|e| {
+                panic!("{}", e);
+            }),
         }
+    }
+
+    fn setup(&mut self) {
+        self.window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
     }
 
     fn cls(&mut self) {
         self.framebuffer = [0; 64 * 32];
+        self.draw();
     }
 
-    fn draw(&self) {
+    fn draw(&mut self) {
+        self.window.update_with_buffer(&self.framebuffer, WIDTH, HEIGHT).unwrap();
+    }
+
+    fn is_key_pressed(&self, index: u8) -> bool {
+        self.keys[index as usize]
+    }
+
+    fn key_down(&mut self, index: u8) {
+        self.keys[index as usize] = true;
+    }
+
+    fn key_up(&mut self, index: u8) {
+        self.keys[index as usize] = false;
+    }
+
+    fn set_keys(&mut self) {
+        for (i, key) in KEY_MAP.iter().enumerate() {
+            if self.window.is_key_down(*key) {
+                self.keys[i] = true;
+            } else {
+                self.keys[i] = false;
+            }
+        }
     }
 }
 
 #[allow(dead_code)]
-const KEY_MAP: [Key; 16] =
-    [
-        Key::Key1, Key::Key2, Key::Key3, Key::Key4,
-        Key::Q, Key::W, Key::E, Key::R,
-        Key::A, Key::S, Key::D, Key::F,
-        Key::Y, Key::X, Key::C, Key::V,
-       
-    ];
-
-#[allow(dead_code)]
-const FONT: [u8; 16 * 5] =
-    [
-        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-        0x20, 0x60, 0x20, 0x20, 0x70, // 1
-        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-        0xF0, 0x80, 0xF0, 0x80, 0x80, // F
-    ];
+const FONT: [u8; 16 * 5] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
 
 fn main() {
-    let mut cpu = Cpu::new();
-    cpu.reset();
-    let width = 64;
-    let height = 32;
+    // Create Chip-8 instance
+    // Initialize all the parts
+    // Load ROM
+    // Start loop
+    // Get Keyboard input
+    // Draw screen
 
-    let mut window = Window::new(
-        "Crusty-8 (Press ESC to exit)",
-        width,
-        height,
-        WindowOptions {
-            resize: false,
-            scale: Scale::X16,
-            ..WindowOptions::default()
-        },
-    )
-    .unwrap_or_else(|e| {panic!("{}", e);});
-
-    // Limit to max ~60 fps update rate
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
-
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        for i in cpu.display.framebuffer.iter_mut() {
+    let mut chip8 = Chip8::new();
+    chip8.reset();
+    chip8.io.setup();
+   
+    while chip8.io.window.is_open() && !chip8.io.window.is_key_down(Key::Escape) {
+        for i in chip8.io.framebuffer.iter_mut() {
             *i = 0xFFFF_FFFF; // write something more funny here!
         }
-
-        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
-        window
-            .update_with_buffer(&cpu.display.framebuffer, width, height)
-            .unwrap();
-        if window.is_hex_pressed(0x0) { println!("It works!") };
+        chip8.io.window.update();
+        chip8.io.set_keys();
+        chip8.execute_cycle();
+        if chip8.io.keys[0] {
+            println!("pressed");
+        } else {
+            println!("UNpressed");
+        }
     }
 }
